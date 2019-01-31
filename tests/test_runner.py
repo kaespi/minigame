1a9193a6cf3afb49065a470198bfc291cbacb152
kaespi <kg@kgmw.ch>
2019-01-31 09:13:34 +0100

    fix the unittests for class "Runner"

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -4,6 +4,7 @@ import context
 
 from config import Gameconfig
 from runner import Runner
+from enumtypes import Direction
 
 
 class TestRunner(unittest.TestCase):
@@ -20,8 +21,8 @@ class TestRunner(unittest.TestCase):
         # moving up
         r.set_position(1.0, 1.5)
         r.vel = 0.2
-        r.move_direction = 0
-        r.update_position(1)
+        r.move_direction = Direction.up
+        r.move(1)
         self.assertTrue(r.x == 1.0)
         self.assertTrue(1.29 < r.y < 1.31)
         self.assertFalse(r.node_visited_x)
@@ -31,8 +32,8 @@ class TestRunner(unittest.TestCase):
         # moving right
         r.set_position(1.5, 1.0)
         r.vel = 0.2
-        r.move_direction = 1
-        r.update_position(1)
+        r.move_direction = Direction.right
+        r.move(1)
         self.assertTrue(1.69 < r.x < 1.71)
         self.assertTrue(r.y == 1.0)
         self.assertFalse(r.node_visited_x)
@@ -42,8 +43,8 @@ class TestRunner(unittest.TestCase):
         # moving down
         r.set_position(1.0, 1.5)
         r.vel = 0.2
-        r.move_direction = 2
-        r.update_position(1)
+        r.move_direction = Direction.down
+        r.move(1)
         self.assertTrue(r.x == 1.0)
         self.assertTrue(1.69 < r.y < 1.71)
         self.assertFalse(r.node_visited_x)
@@ -53,8 +54,8 @@ class TestRunner(unittest.TestCase):
         # moving left
         r.set_position(1.5, 1.0)
         r.vel = 0.2
-        r.move_direction = 3
-        r.update_position(1)
+        r.move_direction = Direction.left
+        r.move(1)
         self.assertTrue(1.29 < r.x < 1.31)
         self.assertTrue(r.y == 1.0)
         self.assertFalse(r.node_visited_x)
@@ -68,8 +69,8 @@ class TestRunner(unittest.TestCase):
         # moving up
         r.set_position(1.0, 1.0)
         r.vel = 0.2
-        r.move_direction = 0
-        r.update_position(1)
+        r.move_direction = Direction.up
+        r.move(1)
         self.assertTrue(r.x == 1.0)
         self.assertTrue(0.79 < r.y < 0.81)
         self.assertFalse(r.node_visited_x)
@@ -79,8 +80,8 @@ class TestRunner(unittest.TestCase):
         # moving right
         r.set_position(1.0, 1.0)
         r.vel = 0.2
-        r.move_direction = 1
-        r.update_position(1)
+        r.move_direction = Direction.right
+        r.move(1)
         self.assertTrue(1.19 < r.x < 1.21)
         self.assertTrue(r.y == 1.0)
         self.assertFalse(r.node_visited_x)
@@ -90,8 +91,8 @@ class TestRunner(unittest.TestCase):
         # moving down
         r.set_position(1.0, 1.0)
         r.vel = 0.2
-        r.move_direction = 2
-        r.update_position(1)
+        r.move_direction = Direction.down
+        r.move(1)
         self.assertTrue(r.x == 1.0)
         self.assertTrue(1.19 < r.y < 1.21)
         self.assertFalse(r.node_visited_x)
@@ -101,8 +102,8 @@ class TestRunner(unittest.TestCase):
         # moving left
         r.set_position(1.0, 1.0)
         r.vel = 0.2
-        r.move_direction = 3
-        r.update_position(1)
+        r.move_direction = Direction.left
+        r.move(1)
         self.assertTrue(0.79 < r.x < 0.81)
         self.assertTrue(r.y == 1.0)
         self.assertFalse(r.node_visited_x)
@@ -115,47 +116,51 @@ class TestRunner(unittest.TestCase):
 
         # moving up
         r.set_position(1.0, 2.1)
+        r.dt_ms_rem = 0
         r.vel = 0.2
-        r.move_direction = 0
-        r.update_position(1)
+        r.move_direction = Direction.up
+        r.move(1)
         self.assertTrue(r.x == 1.0)
-        self.assertTrue(1.89 < r.y < 1.91)
+        self.assertEqual(r.y, 2)
         self.assertEqual(r.node_visited_x, 1)
         self.assertEqual(r.node_visited_y, 2)
-        self.assertEqual(r.dt_ms_rem, 0)
+        self.assertTrue(0.49 < r.dt_ms_rem < 0.51)
 
         # moving right
         r.set_position(1.9, 2.0)
+        r.dt_ms_rem = 0
         r.vel = 0.2
-        r.move_direction = 1
-        r.update_position(1)
-        self.assertTrue(2.09 < r.x < 2.11)
+        r.move_direction = Direction.right
+        r.move(1)
+        self.assertEqual(r.x, 2)
         self.assertTrue(r.y == 2.0)
         self.assertEqual(r.node_visited_x, 2)
         self.assertEqual(r.node_visited_y, 2)
-        self.assertEqual(r.dt_ms_rem, 0)
+        self.assertTrue(0.49 < r.dt_ms_rem < 0.51)
 
         # moving down
         r.set_position(1.0, 0.9)
+        r.dt_ms_rem = 0
         r.vel = 0.2
-        r.move_direction = 2
-        r.update_position(1)
+        r.move_direction = Direction.down
+        r.move(1)
         self.assertTrue(r.x == 1.0)
-        self.assertTrue(1.09 < r.y < 1.11)
+        self.assertEqual(r.y, 1)
         self.assertEqual(r.node_visited_x, 1)
         self.assertEqual(r.node_visited_y, 1)
-        self.assertEqual(r.dt_ms_rem, 0)
+        self.assertTrue(0.49 < r.dt_ms_rem < 0.51)
 
         # moving left
         r.set_position(1.9, 2.0)
+        r.dt_ms_rem = 0
         r.vel = 1.5
-        r.move_direction = 3
-        r.update_position(1)
-        self.assertTrue(0.39 < r.x < 0.41)
+        r.move_direction = Direction.left
+        r.move(1)
+        self.assertEqual(r.x, 1)
         self.assertTrue(r.y == 2.0)
         self.assertEqual(r.node_visited_x, 1)
         self.assertEqual(r.node_visited_y, 2)
-        self.assertEqual(r.dt_ms_rem, 0)
+        self.assertTrue(0.59/1.5 < r.dt_ms_rem < 0.61/1.5)
 
 
 unittest.main()
