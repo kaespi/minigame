2c3b1fc5dad8c73528db192de44e8a0cf9eb3ad7
kaespi <kg@kgmw.ch>
2019-02-06 06:55:22 +0100

    added a "main game class" in game.py to be the central class for the game

--- minigame.py
+++ minigame.py
@@ -1,48 +1,15 @@
 import pygame
-import os
 
-from src.config import Gameconfig
-from src.grid import Grid
-import src.gameaction
-from src.player import Player
-from src.bug import Bug
+import src.game
 
 
 def tmg_main():
     """Main starting point for the game to take off"""
-    cfg = Gameconfig()
     pygame.init()
 
-    # initialize the window
-    screen = pygame.display.set_mode(cfg.screen_size)
-
-    grid = Grid(cfg, screen)
-    grid.read_grid(os.path.join(os.path.dirname(__file__), 'levels', 'level_test1.txt'))
-    grid.check_grid()
-
-    grid.init_squares()
-
-    players = []
-    for player_start in grid.players_start:
-        player = Player(cfg, screen)
-        player.set_grid(grid)
-        player.set_position(player_start[0], player_start[1])
-        players.append(player)
-
-    # initializes the bugs
-    bugs = []
-    bugs_start = grid.get_bugs_start()
-    for bug_start in bugs_start:
-        bug = Bug(cfg, screen)
-        bug.set_grid(grid)
-        bug.set_position(bug_start[0], bug_start[1])
-        bug.find_starting_direction()
-        # only if the bug can move set the velocity
-        if bug.move_direction is not None:
-            bug.vel = cfg.speed
-        bugs.append(bug)
-
-    src.gameaction.run_level(screen, cfg, grid, players, bugs)
+    game = src.game.Game()
+    if game.load_level('level_test1.txt'):
+        game.run_level()
 
 
 tmg_main()
---
+++ src/game.py
@@ -0,0 +1,145 @@
+import pygame
+import os
+
+from src.config import Gameconfig
+from src.grid import Grid
+from src.player import Player
+from src.bug import Bug
+from src.gameaction import bug_catched_player
+from src.enumtypes import Direction
+
+
+class Game():
+    """This is the main class for the game, handling all objects, menus, levels etc."""
+
+    def __init__(self):
+        """Constructor for the Game class"""
+        # loads the configuration (start with default values)
+        self.cfg = Gameconfig()
+
+        # initialize the window
+        self.screen = pygame.display.set_mode(self.cfg.screen_size)
+
+        # initialize the main game objects
+        self.grid = None
+        self.players = []
+        self.bugs = []
+
+    def load_level(self, filename):
+        """Loads a level file"""
+
+        # instantiate a new grid
+        self.grid = Grid(self.cfg, self.screen)
+        # parse the level/grid file
+        self.grid.read_grid(os.path.join(os.path.dirname(__file__), '..', 'levels', filename))
+        # check if the grid is ok
+        success = self.grid.check_grid()
+
+        if success:
+            # initialize the squares between the gridlines
+            self.grid.init_squares()
+
+            # initialize the players
+            self.players = []
+            for player_start in self.grid.players_start:
+                player = Player(self.cfg, self.screen)
+                player.set_grid(self.grid)
+                player.set_position(player_start[0], player_start[1])
+                self.players.append(player)
+
+            # initializes the bugs
+            self.bugs = []
+            bugs_start = self.grid.get_bugs_start()
+            for bug_start in bugs_start:
+                bug = Bug(self.cfg, self.screen)
+                bug.set_grid(self.grid)
+                bug.set_position(bug_start[0], bug_start[1])
+                bug.find_starting_direction()
+                # only if the bug can move set the velocity
+                if bug.move_direction is not None:
+                    bug.vel = self.cfg.speed
+                self.bugs.append(bug)
+
+        else:
+            # the grid is illegal, therefore reset the grid variable
+            self.grid = None
+
+        return success
+
+    def run_level(self):
+        """Runs a level (main while loop during playing)"""
+        if self.grid is None or len(self.players) == 0:
+            # if there's no grid or no player cannot do anything and therefore terminate early
+            return False
+
+        t_ms = pygame.time.get_ticks()
+
+        while True:
+            self.screen.fill(self.cfg.bg_color)
+
+            # needed to keep the events in sync with the system. According to pygame manual should
+            # should be called once per game loop
+            pygame.event.pump()
+
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        quit()
+                    elif event.key == self.cfg.player1_up and len(self.players) >= 1:
+                        self.players[0].command_direction(Direction.up)
+                    elif event.key == self.cfg.player1_right and len(self.players) >= 1:
+                        self.players[0].command_direction(Direction.right)
+                    elif event.key == self.cfg.player1_down and len(self.players) >= 1:
+                        self.players[0].command_direction(Direction.down)
+                    elif event.key == self.cfg.player1_left and len(self.players) >= 1:
+                        self.players[0].command_direction(Direction.left)
+                    elif event.key == self.cfg.player2_up and len(self.players) >= 2:
+                        self.players[1].command_direction(Direction.up)
+                    elif event.key == self.cfg.player2_right and len(self.players) >= 2:
+                        self.players[1].command_direction(Direction.right)
+                    elif event.key == self.cfg.player2_down and len(self.players) >= 2:
+                        self.players[1].command_direction(Direction.down)
+                    elif event.key == self.cfg.player2_left and len(self.players) >= 2:
+                        self.players[1].command_direction(Direction.left)
+
+            t_now_ms = pygame.time.get_ticks()
+            dt_ms = t_now_ms - t_ms
+            t_ms = t_now_ms
+
+            # move the players and bugs on the grid on time step ahead
+            for player in self.players:
+                player.update_position(dt_ms)
+            for bug in self.bugs:
+                bug.update_position(dt_ms, self.players)
+
+            # check if any bug caught any player
+            bug_catched_player(self.bugs, self.players,
+                               .5*self.cfg.bug_size/self.cfg.grid_height,
+                               .5*self.cfg.player_size/self.cfg.grid_height)
+            any_player_alive = False
+            for player in self.players:
+                if not player.caught:
+                    any_player_alive = True
+            if not any_player_alive:
+                print("Level failed")
+                return False
+
+            any_square_not_complete = False
+            for square in self.grid.squares:
+                if not square.is_complete():
+                    any_square_not_complete = True
+            if not any_square_not_complete:
+                print("Level completed")
+                return True
+
+            # draw the grid
+            self.grid.draw_grid()
+
+            # draw the players and bugs
+            for player in self.players:
+                player.draw()
+            for bug in self.bugs:
+                bug.draw()
+
+            pygame.display.update()
