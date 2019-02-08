b2b4180f8dbd220a6f885bde759d8e3eb5d35a84
kaespi <kg@kgmw.ch>
2019-02-08 23:15:26 +0100

    nicer game speed and appearance

--- src/config.py
+++ src/config.py
@@ -14,7 +14,7 @@ class Gameconfig():
         self.grid_color_fresh = (140, 140, 140)     # color of the grid, when it's not yet done (RGB)
         self.grid_color_done = (30, 0, 120)         # color of the grid, when it's done (RGB)
         self.grid_color_complete = (30, 0, 120)     # color of the grid, when it's completed (RGB)
-        self.grid_width = 3                         # width of the grid lines [pixel]
+        self.grid_width = 5                         # width of the grid lines [pixel]
         self.grid_height = 50                       # length of the grid lines [pixel]
 
         # squares
@@ -22,16 +22,16 @@ class Gameconfig():
         self.square_color_complete = (50, 50, 50)   # color of the square when it's completed (RGB)
 
         # players
-        self.player_size = 8                        # diameter of the player [pixel]
+        self.player_size = 10                       # diameter of the player [pixel]
         self.player_color = (0, 130, 0)             # color of the player (circle) (RGB)
         self.player_color_caught = (80, 80, 80)     # color of the player after it was caught (RGB)
 
         # bugs
-        self.bug_size = 10                          # diameter of the bug [pixel]
+        self.bug_size = 12                          # diameter of the bug [pixel]
         self.bug_color = (150, 30, 0)               # color of the bug (circle) (RGB)
 
         # game behavior
-        self.speed = 0.002                          # movement speed [pixel/ms]
+        self.speed = 0.0019                         # movement speed [pixel/ms]
 
         # controls
         self.player1_up = pygame.K_UP               # player 1: key up
