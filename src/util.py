dee5d14f1fc6a0d3c60a468dfed287033a8ceec6
kaespi <kg@kgmw.ch>
2019-03-08 07:39:44 +0100

    added scanning for level files in the "levels" subfolder. Unittest included

--- src/util.py
+++ src/util.py
@@ -1,3 +1,5 @@
+import os
+
 from src.enumtypes import Direction
 
 
@@ -16,3 +18,59 @@ def get_reverse_direction(direction):
             direction = None
 
     return direction
+
+
+def scan_for_levels(num_players=1):
+    """Scans the levels folder for level files"""
+    print("#players="+str(num_players))
+    path_to_file = os.path.dirname(os.path.realpath(__file__))
+    path_to_levels = os.path.join(path_to_file, '..', 'levels')
+    files_found = os.listdir(path_to_levels)
+    level_files = []
+    for file in files_found:
+        if is_valid_level_file(file, num_players):
+            level_files.append(file)
+
+    return sorted(level_files)
+
+
+def is_valid_level_file(filename, num_players=None):
+    """Returns True if the filename is a valid level filename"""
+    # a level file should follow a naming pattern as level_Np_M.txt, with
+    # N the number of players and M the level number
+
+    valid_filename = True
+
+    # 1. check the length of the filename
+    if len(filename) < 14:
+        valid_filename = False
+
+    # 2. check for the file ending
+    if valid_filename and filename[-4:] != '.txt':
+        valid_filename = False
+
+    # 3. check for the pattern
+    if valid_filename:
+        filename = filename[:-4]
+        parts = filename.split('_')
+        # should contain at least two underscores
+        if len(parts) >= 3:
+            # first part should be "level"
+            if parts[0] != 'level':
+                valid_filename = False
+            # last part should be a number (level number)
+            if not parts[-1].isdigit():
+                valid_filename = False
+            # second last part should be a number with "p" appended
+            if parts[-2][-1] != 'p':
+                valid_filename = False
+            if not parts[-2][:-1].isdigit():
+                valid_filename = False
+            # the number of players argument should match the one selected
+            if valid_filename and num_players is not None:
+                if int(parts[-2][:-1]) != num_players:
+                    valid_filename = False
+        else:
+            valid_filename = False
+
+    return valid_filename
--- tests/test_util.py
+++ tests/test_util.py
@@ -16,5 +16,59 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(Direction.right, util.get_reverse_direction(Direction.left))
         self.assertEqual(None, util.get_reverse_direction(None))
 
+    def test_filename_validation(self):
+        """Tests the scanning for level files in function is_valid_level_file()"""
+        # 1. no number of players argument
+        # invalid level filenames
+        self.assertFalse(util.is_valid_level_file(''))
+        self.assertFalse(util.is_valid_level_file('.txt'))
+        self.assertFalse(util.is_valid_level_file('abc.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def_ghi.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def_ghi_jkl.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def_001.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_dep_001.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_1p_1.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_9p_9.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_01p_001.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_99p_999.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def_01p_001.txt'))
+        self.assertFalse(util.is_valid_level_file('abc_def_99p_999.txt'))
+
+        # valid filenames
+        self.assertTrue(util.is_valid_level_file('level_1p_1.txt'))
+        self.assertTrue(util.is_valid_level_file('level_9p_9.txt'))
+        self.assertTrue(util.is_valid_level_file('level_01p_001.txt'))
+        self.assertTrue(util.is_valid_level_file('level_99p_999.txt'))
+        self.assertTrue(util.is_valid_level_file('level_def_01p_001.txt'))
+        self.assertTrue(util.is_valid_level_file('level_def_99p_999.txt'))
+
+        # 2. with number of players argument
+        # invalid level filenames
+        self.assertFalse(util.is_valid_level_file('', 1))
+        self.assertFalse(util.is_valid_level_file('.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def_ghi.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def_ghi_jkl.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def_001.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_dep_001.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_1p_1.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_9p_9.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_01p_001.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_99p_999.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def_01p_001.txt', 1))
+        self.assertFalse(util.is_valid_level_file('abc_def_99p_999.txt', 1))
+        self.assertFalse(util.is_valid_level_file('level_9p_9.txt', 1))
+
+        # valid filenames
+        self.assertTrue(util.is_valid_level_file('level_1p_1.txt', 1))
+        self.assertTrue(util.is_valid_level_file('level_01p_1.txt', 1))
+        self.assertTrue(util.is_valid_level_file('level_001p_1.txt', 1))
+        self.assertTrue(util.is_valid_level_file('level_01p_001.txt', 1))
+        self.assertTrue(util.is_valid_level_file('level_99p_999.txt', 99))
+        self.assertTrue(util.is_valid_level_file('level_def_5p_001.txt', 5))
+        self.assertTrue(util.is_valid_level_file('level_def_10p_999.txt', 10))
+
 
 unittest.main()
